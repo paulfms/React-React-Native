5057969418767b5f3d78e557378ebe34328f80f7
Paul Anderson <pauldevop@gmail.com>
2020-04-16 18:53:22 -0300

    Commit no Pacote

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,32 @@
+//Buscar todos devs num raio 10km
+//Filtrar por tecnologia
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray'); 
+
+module.exports = {
+async index(request, response) {
+    //Buscar Devs em um raio de 10km
+    //Filtrar por Tecnologias que
+    const { latitude, longitude, techs } = request.query;
+
+    const techsArray = parseStringAsArray(techs);
+
+    const devs = await Dev.find({
+        techs: {
+            $in: techsArray,
+        },
+        location: {
+            $near: {
+                $geometry: {
+                    type: 'Point',
+                    coordinates: [longitude, latitude],
+                },
+                $maxDistance: 10000,
+            },
+        },
+
+    });
+
+    return response.json({ devs });
+}
+}
---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
+
+mongoose.connect('mongodb+srv://pauldevop:paul@cluster0-jac2z.mongodb.net/dbDevRadar?retryWrites=true&w=majority', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
+
